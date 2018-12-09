bf9c968a9482aca85a129d07ea13ec8d0e482d0b
Ahmet Can <ahmetcanguven44@gmail.com>
2018-12-09 17:23:06 +0300

    init skeleton

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "transform": {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  "testMatch": [
+    "**/test/*.+(ts|tsx|js)"
+  ],
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js"
+  ]
+};
--- test/core.spec.ts
+++ test/core.spec.ts
@@ -1,13 +1,24 @@
-import {describe, it} from "mocha";
+import "jest";
 import {expect} from "chai";
 import {Core} from "../lib";
 
+let core: Core;
+
 describe("[core.ts]", () => {
+  beforeEach(() => {
+    core = new Core();
+  });
+
   it("should create new Core", () => {
     // Act
     const core = new Core();
 
     // Assert
     expect(core).to.be.instanceOf(Core);
+  });
+
+  it("should use app-root-path library to pick default location for project root", () => {
+    // Assert
+    expect(core.rootPath).to.be.an('string');
   });
 });
